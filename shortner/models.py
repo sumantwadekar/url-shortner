18e267689607cf5a34a50d372c13e905865207a2
Sumant Wadekar <sumant@mortgagekart.com>
2025-02-02 19:35:03 +0530

    urls model class, database settings, migration - I

--- shortner/models.py
+++ shortner/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class URLs(models.Model):
+    short_url = models.CharField(
+        verbose_name="Shortened URL for customer",
+        max_length=6,
+    )
+    long_url = models.TextField(verbose_name="Long URL provided by customer")
+
+    class Meta:
+        db_table = 'urls'
